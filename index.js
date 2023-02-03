cfec3ba7b261fcbc6d48dd6a996d11bdd3205e48
Hamad Bakeel <90466313+HamadBakeel@users.noreply.github.com>
2023-02-03 18:38:55 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(callback){
+  callback();
+}
+function returnsANamedFunction() {
+  function namedFunction() {
+    console.log("I'm named function")
+  }
+
+  return namedFunction
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {
+    console.log("I'm an anonymous function")
+  }
+
+}
